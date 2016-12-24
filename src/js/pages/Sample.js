ed8b18991f219b9b3536c5cdc5883e87bf794674
gafuchs <gafuchs@gmail.com>
2016-12-24 20:43:04 -0200

    Sample side navbar update;

--- src/js/pages/Sample.js
+++ src/js/pages/Sample.js
@@ -26,54 +26,53 @@ export default class Sample extends React.Component {
     };
 
     return (
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-sm-3 col-lg-2">
-            <nav class="navbar navbar-default navbar-fixed-side" style={navStyle}>
-              <div class="container">
-                <div class="navbar-header">
-                  <button type="button" class="navbar-toggle" onClick={this.toggleCollapse.bind(this)} >
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                  </button>
-                  <a class="navbar-brand" href="./">BS3 Side Navbar</a>
-                </div>
-                <div class={"navbar-collapse " + navClass}>
-                  <ul class="nav navbar-nav">
-                    <li class="active">
-                      <a href="./">Left (Default Theme)</a>
-                    </li>
-                    <li class="">
-                      <a href="inverse.html">Right (Inverse Theme)</a>
-                    </li>
-                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Dropdown <b class="caret"></b></a><ul class="dropdown-menu"><li><a href="#">Sub-page 1</a></li>
-                    <li><a href="#">Sub-page 2</a></li>
-                    <li class="divider"></li>
-                    <li class="dropdown-header">Dropdown Header</li>
-                    <li><a href="#">Sub-page 3</a></li>
-                    </ul></li>
-                  </ul>
-                  <form class="navbar-form navbar-left">
-                    <div class="form-group">
-                      <input class="form-control" placeholder="Search"/>
-                    </div>
-                    <button class="btn btn-default">Search</button>
-                  </form>
-                  <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Page 4</a></li>
-                  </ul>
-                  <button class="btn btn-default navbar-btn">Button</button>
-                  <p class="navbar-text">
-                    Made by 
-                    <a href="http://www.samrayner.com">Sam Rayner</a>
-                  </p>
-                </div>
-              </div>
-            </nav>
+      <div>
+        <nav class="navbar navbar-default navbar-fixed-side col-md-2" style={navStyle}>
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" onClick={this.toggleCollapse.bind(this)} >
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div>
           </div>
-        </div>
+          <div class={"navbar-collapse " + navClass}>
+          <ul class="nav navbar-nav">
+                  <li class="active">
+                    <a href="./">Left (Default Theme)</a>
+                  </li>
+                  <li class="">
+                    <a href="inverse.html">Right (Inverse Theme)</a>
+                  </li>
+                  <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Dropdown <b class="caret"></b></a><ul class="dropdown-menu"><li><a href="#">Sub-page 1</a></li>
+                  <li><a href="#">Sub-page 2</a></li>
+                  <li class="divider"></li>
+                  <li class="dropdown-header">Dropdown Header</li>
+                  <li><a href="#">Sub-page 3</a></li>
+                  </ul></li>
+            </ul>
+            <ul class="nav nav-sidebar">
+              <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+              <li><a href="#">Reports</a></li>
+              <li><a href="#">Analytics</a></li>
+              <li><a href="#">Export</a></li>
+            </ul>
+            <ul class="nav nav-sidebar">
+              <li><a href="">Nav item</a></li>
+              <li><a href="">Nav item again</a></li>
+              <li><a href="">One more nav</a></li>
+              <li><a href="">Another nav item</a></li>
+              <li><a href="">More navigation</a></li>
+            </ul>
+            <ul class="nav nav-sidebar">
+              <li><a href="">Nav item again</a></li>
+              <li><a href="">One more nav</a></li>
+              <li><a href="">Another nav item</a></li>
+            </ul>
+          </div>
+        </nav>
       </div>
     );
   }
