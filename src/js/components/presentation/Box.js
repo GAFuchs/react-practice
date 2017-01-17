144a2b637c5ec2297ab9c8e29499c0124ba5ed9c
gafuchs <gafuchs@gmail.com>
2017-01-17 18:12:04 -0200

    Box component creation;

---
+++ src/js/components/presentation/Box.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default class Box extends React.Component {
+  constructor(props) {
+    super();
+  }
+
+  buildImg() {
+
+  }
+
+  render() {
+
+    const boxStyle = {
+      height: "120px",
+      marginBottom: "20px"
+    };
+
+    const imgDivStyle = {
+      padding: "0px",
+      height: "100%",
+      borderBottom: "1px solid #C7C8C9",
+    };
+
+    const imgStyle = {
+      width: "100%",
+      height: "100%",
+    }
+
+    const textDivStyle = {
+      textAlign: "left",
+      backgroundColor: "#FFFFFF",
+      height: "100%",
+      borderBottom: "1px solid #C7C8C9",
+    };
+
+    return (
+      <div>
+        <div class="col-xs-12 col-md-4" style={boxStyle}>
+          <div class="col-xs-3 col-md-5" style={imgDivStyle}>
+            <img src={this.props.imgSrc} style={imgStyle}/>
+          </div>
+          <div class="col-xs-9 col-md-7" style={textDivStyle}>
+            <h4>About</h4>
+            <p>What is this page, our clients and specialties.</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/js/pages/samplePages/Presentation.js
+++ src/js/pages/samplePages/Presentation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Carousel from "../../components/presentation/Carousel";
+import Box from "../../components/presentation/Box";
 
 export default class Presentation extends React.Component {
   constructor(props) {
@@ -23,6 +24,7 @@ export default class Presentation extends React.Component {
 
     const boxStyle = {
       height: "100px",
+      marginBottom: "20px"
     };
 
     const imgDivStyle = {
@@ -45,48 +47,21 @@ export default class Presentation extends React.Component {
 
     return (
       <div>
-
         <Carousel
           imgList={imgList}/>
 
-        <div class="col-md-12 container" style={containerStyle}>
+        <div class="col-xs-12 col-md-12 container" style={containerStyle}>
           <h1>Presentation Page</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
           <div class="row">
-            <div class="col-md-4" style={boxStyle}>
-              <div class="col-md-4" style={imgDivStyle}>
-                <img src="../../../img/about.jpg" style={imgStyle}/>
-              </div>
-              <div class="col-md-8" style={textDivStyle}>
-                <h4>About</h4>
-                <p>What is this page, our clients and specialties.</p>
-              </div>
-            </div>
 
-            <div class="col-md-4" style={boxStyle}>
-              <div class="col-md-4" style={imgDivStyle}>
-                <img src="../../../img/about.jpg" style={imgStyle}/>
-              </div>
-              <div class="col-md-8" style={textDivStyle}>
-                <h4>About</h4>
-                <p>What is this page, our clients and specialties.</p>
-              </div>
-            </div>
+            <Box imgSrc="../../../img/about.jpg"/>
+            <Box imgSrc="../../../img/about.jpg"/>
+            <Box imgSrc="../../../img/about.jpg"/>
 
-            <div class="col-md-4" style={boxStyle}>
-              <div class="col-md-4" style={imgDivStyle}>
-                <img src="../../../img/about.jpg" style={imgStyle}/>
-              </div>
-              <div class="col-md-8" style={textDivStyle}>
-                <h4>About</h4>
-                <p>What is this page, our clients and specialties.</p>
-              </div>
-            </div>
           </div>
-
         </div>
-
       </div>
     );
   }
