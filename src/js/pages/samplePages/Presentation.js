ab27e5f08598386727d149fbbe3a112ac4b71cc2
gafuchs <gafuchs@gmail.com>
2017-01-31 19:24:02 -0200

    Presentation page footer;

--- src/js/pages/samplePages/Presentation.js
+++ src/js/pages/samplePages/Presentation.js
@@ -9,7 +9,7 @@ export default class Presentation extends React.Component {
 
   render() {
 
-    const containerStyle = {
+    const containerBoxStyle = {
       textAlign: "center",
       backgroundColor: "#EEEEF2",
       display: "inline",
@@ -17,7 +17,13 @@ export default class Presentation extends React.Component {
 
     const boxDivStyle = {
       marginTop: "50px",
-    }
+    };
+
+    const containerBottomStyle = {
+      textAlign: "left",
+      display: "inline",
+      marginTop: "50px",
+    };
 
     const imgList = [
       "../../../img/carousel/architecture.jpg",
@@ -31,7 +37,7 @@ export default class Presentation extends React.Component {
         <Carousel
           imgList={imgList}/>
 
-        <div class="col-xs-12 col-md-12 container" style={containerStyle}>
+        <div class="col-xs-12 col-md-12 container" style={containerBoxStyle}>
           <h1>Presentation Page</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
@@ -50,6 +56,24 @@ export default class Presentation extends React.Component {
               text="Where to find, contact module and more."
               imgSrc="../../../img/contacts.jpg"/>
 
+          </div>
+        </div>
+        <div class="col-xs-12 col-md-12 container-fluid" style={containerBottomStyle}>
+          <div class="col-xs-12 col-md-4">
+            <h5>
+              <b>Gabriel Fuchs</b>
+              <small> Web App Developer</small>
+            </h5>
+            <p>email - gafuchs@gmail.com</p>
+          </div>
+          <div class="col-xs-12 col-md-2">
+
+          </div>
+          <div class="col-xs-12 col-md-4">
+            <h5><b>From the Page</b></h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor... <a>Continue</a></p>
+          </div>
+          <div class="col-xs-12 col-md-1">
           </div>
         </div>
       </div>
